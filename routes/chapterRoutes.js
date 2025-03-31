369e8771d95049e05e34bc09261315f5fe5dccf1
Long Vu <144806953+long-vux@users.noreply.github.com>
2025-03-31 11:20:00 +0700

    Delete routes directory (dư thừa)

--- routes/chapterRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createChapter, getChaptersByStory, addChapterImage } = require('../src/controllers/chapterController');
-
-router.post('/', createChapter);
-router.get('/story/:story_id', getChaptersByStory);
-router.post('/:chapter_id/images', addChapterImage);
-
-module.exports = router;
